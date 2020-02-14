e0da65ce935e1cbdd56a50262392486ffeb0985d
BRAINIFII <bangerakritik@gmail.com>
2020-02-14 23:18:24 +0530

    Added Arduinodroid Directory

---
+++ Digispark/Payloads/Normal/digi_LockPrank/digi_LockPrank.ino
@@ -0,0 +1,21 @@
+//This DigiSpark script doesnot gives time to enter your password.
+//Made by me :) ------  BRAINIFII
+
+#define KEY_BACKSPACE 42
+#include "DigiKeyboard.h"
+void setup() {
+  pinMode(1, OUTPUT);
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_L,MOD_GUI_LEFT);
+  DigiKeyboard.delay(1000);
+}
+
+void loop() {
+  digitalWrite(1, LOW);
+  DigiKeyboard.delay(500);
+  digitalWrite(1, HIGH);
+  DigiKeyboard.sendKeyStroke(KEY_A,MOD_CONTROL_LEFT);
+  DigiKeyboard.delay(50);
+  DigiKeyboard.sendKeyStroke(KEY_BACKSPACE);
+}
